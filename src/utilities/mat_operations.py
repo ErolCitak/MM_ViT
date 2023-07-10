7e50819a6823df73d08242a4807a8580af5dc0f2
ErolCitak <citak.erol@gmail.com>
2023-07-10 15:55:53 +0300

    Matrix operations are added

--- src/utilities/mat_operations.py
+++ src/utilities/mat_operations.py
@@ -3,3 +3,6 @@ def sum(num_1, num_2):
 	
 def sub(num_1, num_2):
 	return num_1 - num_2
+
+def isPostive(num_1):
+    return num_1 > 0
---
+++ src/utilities/matrix_operations.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def matrix_multiply(mat_1, mat_2):
+    return np.matmul(mat_1, mat_2)
